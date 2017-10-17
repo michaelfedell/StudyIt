deff7639c7d7a68e26f2c652aebc2539dbf3f67f
Michael Fedell <s.michaelfedell@gmail.com>
2017-10-17 12:50:34 -0500

    Begin NoNav
    Having lots of issues with Alerts but have removed navigator layout and begun single component build

--- App.js
+++ App.js
@@ -4,22 +4,155 @@
  * @flow
  */
 
- import PropTypes from 'prop-types';
- import React, { Component } from 'react';
- import { NavigatorIOS, Text } from 'react-native';
- import questionScene from "./questionScene";
- import answerScene from "./questionScene"; 
+import React, { Component } from 'react';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Icon,
+  List,
+  ListItem,
+  Text,
+  Left,
+  Right,
+  Body,
+  Footer,
+  FooterTab,
+} from "native-base";
 
+ import AlertIOS from 'react-native';
+ 
+ import styles from "./styles";
+ 
  export default class App extends Component {  
+   constructor(props) {
+     super(props);
+     this.state = {
+       radio1: false,
+       radio2: false,
+       radio3: false,
+       radio4: false,
+       questionIndex: 0,
+     };
+   }
+ 
+   checkAnswer(ans) {
+     var key = 2
+     if (ans == key) {
+       AlertIOS.alert('Correct!');
+     }
+     else {
+       AlertIOS.alert('Sorry, that is incorrect');
+     }
+   }
+ 
+   toggleRadio1() {
+     this.setState({
+       radio1: true,
+       radio2: false,
+       radio3: false,
+       radio4: false
+     });
+   }
+ 
+   toggleRadio2() {
+     this.setState({
+       radio1: false,
+       radio2: true,
+       radio3: false,
+       radio4: false
+     });
+   }
+ 
+   toggleRadio3() {
+     this.setState({
+       radio1: false,
+       radio2: false,
+       radio3: true,
+       radio4: false
+     });
+   }
+ 
+   toggleRadio4() {
+     this.setState({
+       radio1: false,
+       radio2: false,
+       radio3: false,
+       radio4: true
+     });
+   }
+ 
    render() {
      return (
-       <NavigatorIOS
-         initialRoute={{
-           component: questionScene,
-           title: 'Question',
-         }}
-         style={{flex: 1}}
-       />
+       <Container style={styles.container}>
+         <Header>
+           <Body>
+             <Title>Question {this.state.questionIndex + 1}</Title>
+           </Body>
+         </Header>
+ 
+         <Content>
+           <Text style={styles.question}>Question Content Here</Text>
+           
+           <ListItem
+             selected={this.state.radio1}
+             onPress={() => this.toggleRadio1()}
+             onPress={() => this.checkAnswer(2)}
+           >
+             <Text>Answer 1</Text>
+             <Right>
+               <Button />
+             </Right>
+           </ListItem>
+ 
+           <ListItem
+             selected={this.state.radio2}
+             onPress={() => this.toggleRadio2()}
+             onPress={() => AlertIOS.alert('Test')}
+           >
+             <Text>Answer 2</Text>
+             <Right>
+               <Button />
+             </Right>
+           </ListItem>
+ 
+           <ListItem
+             selected={this.state.radio3}
+             onPress={() => this.toggleRadio3()}
+           >
+             <Text>Answer 3</Text>
+             <Right>
+               <Button />
+             </Right>
+           </ListItem>
+ 
+           <ListItem
+             selected={this.state.radio4}
+             onPress={() => this.toggleRadio4()}
+           >
+             <Text>Answer 4</Text>
+             <Right>
+               <Button />
+             </Right>
+           </ListItem>
+         </Content>
+ 
+         <Footer>
+           <FooterTab>
+             <Button>
+               <Text>Button 1</Text>
+             </Button>
+             <Button>
+               <Text>Button 2</Text>
+             </Button>
+             <Button>
+               <Text>Button 3</Text>
+             </Button>
+           </FooterTab>
+         </Footer>
+       </Container>
      );
    }
  }
